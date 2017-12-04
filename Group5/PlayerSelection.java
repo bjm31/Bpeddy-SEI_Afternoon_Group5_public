b9db409a6aeee6d8b03121908c00aaf86f244700
Bryan Mort <32080274+bjm31@users.noreply.github.com>
2017-12-04 16:50:12 -0600

    Update PlayerSelection.java

--- Group5/PlayerSelection.java
+++ Group5/PlayerSelection.java
@@ -1,11 +1,22 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Label;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
+import java.io.File;
 
 public class PlayerSelection {
-
+	
   public JFrame selectionFrame;
 
   public JButton choice1;
@@ -23,7 +34,7 @@ public class PlayerSelection {
   public PlayerSelection(Dimension frameSize) {
 	  
 	  this.frameSize = frameSize;
-	  setSelectionFrame(framesize);
+	  setSelectionFrame(frameSize);
   }
 
   public JFrame getSelectionFrame() {
@@ -33,10 +44,39 @@ public class PlayerSelection {
 
   public void setSelectionFrame(Dimension frameSize) {
 	  selectionFrame = new JFrame("Player Selection");
-	  
+	  selectionFrame.setSize(frameSize);
 	  selectionFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	  selectionFrame.setBackground(Color.black);
-	  selectionFrame.setPreferredSize(frameSize);
+	  
+	  JButton choice1 = new JButton("Select");
+	  choice1.setSize(100,200);
+	  JButton choice2 = new JButton("Select");
+	  JButton choice3 = new JButton("Select");
+	  
+	  JPanel selectionPanel1 = new JPanel(new GridLayout(2,3, 50, 1));
+	  selectionPanel1.setBackground(java.awt.Color.black);
+	  JLabel label1 = new JLabel();
+	  JLabel label2 = new JLabel();
+	  JLabel label3 = new JLabel();
+	  try {
+		  label1 = new JLabel((Icon)new ImageIcon(ImageIO.read(new File ("C:\\Users\\bjm31\\OneDrive\\YODA-FIGURE.png"))));
+		  label2 = new JLabel((Icon)new ImageIcon(ImageIO.read(new File ("C:\\Users\\bjm31\\OneDrive\\YODA-FIGURE.png"))));
+		  label3 = new JLabel((Icon)new ImageIcon(ImageIO.read(new File ("C:\\Users\\bjm31\\OneDrive\\YODA-FIGURE.png"))));
+	  }
+	  catch (IOException e)
+	  {
+		  e.printStackTrace();
+	  }
+	  selectionPanel1.add(label1);
+	  selectionPanel1.add(label2);
+	  selectionPanel1.add(label3);
+	  selectionPanel1.add(choice1);
+	  selectionPanel1.add(choice2);
+	  selectionPanel1.add(choice3);
+	  
+	  selectionFrame.add(selectionPanel1);
+	  
+	  System.out.println(selectionFrame.getSize().toString());
+	  
   }
 
   public JButton getChoice1() {
@@ -44,6 +84,7 @@ public class PlayerSelection {
   }
 
   public void setChoice1(JButton jButton) {
+	  
   }
 
   public JButton getChoice2() {
