08c3a394513cddccbc3f232c1ed39e06f73de443
NicholasLookabaugh <32078028+NicholasLookabaugh@users.noreply.github.com>
2017-12-04 20:40:29 -0600

    Update ShipHubMenu.java

--- Group5/ShipHubMenu.java
+++ Group5/ShipHubMenu.java
@@ -1,24 +1,45 @@
+import java.awt.Dimension;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
 
 public class ShipHubMenu {
 
-  private BufferedImage shipHubImage;
+
 
   private MissionSelection missionSelection;
 
   private WorldStandingMenu worldStandingMenu;
 
   private ForceSelection forceSelection;
+  
+  private Dimension frameSize;
+  
+  private JFrame shipHubFrame;
 
-  public void ShipHubMenu(BufferedImage shipHubImage, MissionSelection missionSelection, WorldStandingMenu worldStandingMenu, 
-                          ForceSelection forceSelection) {
+  public ShipHubMenu(Dimension frameSize) {
+		setShipHubMenuFrame(frameSize);
+	}
+  public void setShipHubMenuFrame(Dimension frameSize) {
+	  shipHubFrame = new JFrame("Player Selection");
+	  shipHubFrame.setSize(frameSize);
+	  shipHubFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	  try {
+			shipHubFrame.setContentPane(new JLabel((Icon) new ImageIcon(ImageIO.read(new File("ship.jpg")))));
+	     	} catch (IOException e) {
+	    		e.printStackTrace();
+	   }
+	  shipHubFrame.pack();
+	  shipHubFrame.setVisible(true);
   }
 
-  public BufferedImage getShipHubImage() {
-    return null;
-  }
 
-  public void setShipHubMenu(BufferedImage shipHubMenu) {
-  }
 
   public MissionSelection getMissionSelection() {
     return null;
