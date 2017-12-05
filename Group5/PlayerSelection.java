b7fc9bc6c916cf33defc5c875d383af96a8f4e67
NicholasLookabaugh <32078028+NicholasLookabaugh@users.noreply.github.com>
2017-12-05 12:12:07 -0600

    Update PlayerSelection.java

--- Group5/PlayerSelection.java
+++ Group5/PlayerSelection.java
@@ -1,11 +1,8 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
