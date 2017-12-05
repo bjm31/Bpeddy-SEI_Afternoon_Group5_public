7e5d6d580aebad695ffd4933f673d809d83c2e09
NicholasLookabaugh <32078028+NicholasLookabaugh@users.noreply.github.com>
2017-12-05 11:48:20 -0600

    Update MissionSelection.java

--- Group5/MissionSelection.java
+++ Group5/MissionSelection.java
@@ -36,6 +36,7 @@ public class MissionSelection {
   private JLabel region5Title;
   
   private Dimension frameSize;
+  Dimension maxButtonSize;
   
   private JFrame missionSelectionFrame;
 
@@ -58,40 +59,7 @@ public class MissionSelection {
 
 	  missionSelectionFrame.setVisible(true);
 	  missionSelectionFrame.setSize(frameSize);
-	  
-	  region1Mission1Button = new JButton("Mission 1");
-	  region1Title = new JLabel("Region 1");
-	  region1Title.setFont(new Font(region1Title.getName(), Font.BOLD, 18));
-	  region1Title.setForeground(java.awt.Color.white);
-	  
-	  region2Mission1Button = new JButton("Mission 1");
-	  region2Title = new JLabel("Region 2");
-	  region2Title.setFont(new Font(region2Title.getName(), Font.BOLD, 18));
-	  region2Title.setForeground(java.awt.Color.white);
-	  
-	  region3Mission1Button = new JButton("Mission 1");
-	  region3Title = new JLabel("Region 3");
-	  region3Title.setFont(new Font(region3Title.getName(), Font.BOLD, 18));
-	  region3Title.setForeground(java.awt.Color.white);
-	  
-	  region4Mission1Button = new JButton("Mission 1");
-	  region4Title = new JLabel("Region 4");
-	  region4Title.setFont(new Font(region4Title.getName(), Font.BOLD, 18));
-	  region4Title.setForeground(java.awt.Color.white);
-	  
-	  region5Mission1Button = new JButton("Mission 1");
-	  region5Title = new JLabel("Region 5");
-	  region5Title.setFont(new Font(region5Title.getName(), Font.BOLD, 18));
-	  region5Title.setForeground(java.awt.Color.white);
-	  
-	  Dimension maxButtonSize = region1Mission1Button.getPreferredSize();
-	  
-	  region1Mission1Button.addActionListener(new ActionListener() {
-		  public void actionPerformed(ActionEvent e) {
-			  region1Mission1ButtonPressed(); 
-	  		} 
-	  } );
-	  
+
 	  title = new JLabel("Mission Selection");
 	  title.setFont(new Font(title.getName(), Font.BOLD, 25));
 	  title.setForeground(java.awt.Color.white);
@@ -101,46 +69,11 @@ public class MissionSelection {
 	  title.setBounds((int) (frameSize.getWidth() / 2 - titleSize.getWidth() / 2), (int) frameSize.getHeight() / 25,
 				(int) titleSize.getWidth(), (int) titleSize.getHeight());
 	  
-	  region1MissionPanel = new JPanel();
-	  region1MissionPanel.setOpaque(false);
-	  region1MissionPanel.add(region1Title);
-	  region1MissionPanel.add(region1Mission1Button);
-	  region1MissionPanel.setBounds((int) (frameSize.getWidth() / 8 - maxButtonSize.getWidth() / 2), (int) (frameSize.getHeight() / 8 - maxButtonSize.getHeight() / 2),
-	    		(int) (maxButtonSize.getWidth()), (int) ((maxButtonSize.getHeight())) * 3);
-	  
-	  region2MissionPanel = new JPanel();
-	  region2MissionPanel.setOpaque(false);
-	  region2MissionPanel.add(region2Title);
-	  region2MissionPanel.add(region2Mission1Button);
-	  region2MissionPanel.setBounds((int) (frameSize.getWidth() / 5 - maxButtonSize.getWidth() / 2), (int) (frameSize.getHeight() / 2 - maxButtonSize.getHeight() / 2),
-	    		(int) (maxButtonSize.getWidth()), (int) ((maxButtonSize.getHeight())) * 3);
-	  
-	  region3MissionPanel = new JPanel();
-	  region3MissionPanel.setOpaque(false);
-	  region3MissionPanel.add(region3Title);
-	  region3MissionPanel.add(region3Mission1Button);
-	  region3MissionPanel.setBounds((int) (frameSize.getWidth() / 2 - maxButtonSize.getWidth() / 2 - 30), (int) (frameSize.getHeight() / 2 - maxButtonSize.getHeight() / 2 - 50),
-	    		(int) (maxButtonSize.getWidth()), (int) ((maxButtonSize.getHeight())) * 3);
-	  
-	  region4MissionPanel = new JPanel();
-	  region4MissionPanel.setOpaque(false);
-	  region4MissionPanel.add(region4Title);
-	  region4MissionPanel.add(region4Mission1Button);
-	  region4MissionPanel.setBounds((int) (frameSize.getWidth() / 2 - maxButtonSize.getWidth() / 2 + 100), (int) (frameSize.getHeight() / 8 - maxButtonSize.getHeight() / 2),
-	    		(int) (maxButtonSize.getWidth()), (int) ((maxButtonSize.getHeight())) * 3);
-	  
-	  region5MissionPanel = new JPanel();
-	  region5MissionPanel.setOpaque(false);
-	  region5MissionPanel.add(region5Title);
-	  region5MissionPanel.add(region5Mission1Button);
-	  region5MissionPanel.setBounds((int) (frameSize.getWidth() - maxButtonSize.getWidth() / 2 - 300), (int) (frameSize.getHeight() / 2 - maxButtonSize.getHeight() / 2),
-	    		(int) (maxButtonSize.getWidth()), (int) ((maxButtonSize.getHeight())) * 3);
-	  
-	  missionSelectionFrame.add(region1MissionPanel);
-	  missionSelectionFrame.add(region2MissionPanel);
-	  missionSelectionFrame.add(region3MissionPanel);
-	  missionSelectionFrame.add(region4MissionPanel);
-	  missionSelectionFrame.add(region5MissionPanel);
+	  missionSelectionFrame.add(addRegion1MissionPanel());
+	  missionSelectionFrame.add(addRegion2MissionPanel());
+	  missionSelectionFrame.add(addRegion3MissionPanel());
+	  missionSelectionFrame.add(addRegion4MissionPanel());
+	  missionSelectionFrame.add(addRegion5MissionPanel());
 	  missionSelectionFrame.add(title);
 	  
 	  System.out.println(missionSelectionFrame.getSize().toString());
@@ -150,6 +83,135 @@ public class MissionSelection {
   {
 	  missionSelectionFrame.dispose();
 	  new MissionSelection(frameSize);
+  }
+  
+  public void region2Mission1ButtonPressed()
+  {
+	  missionSelectionFrame.dispose();
+	  new MissionSelection(frameSize);
+  }
+  
+  public void region3Mission1ButtonPressed()
+  {
+	  missionSelectionFrame.dispose();
+	  new MissionSelection(frameSize);
+  }
+  
+  public void region4Mission1ButtonPressed()
+  {
+	  missionSelectionFrame.dispose();
+	  new MissionSelection(frameSize);
+  }
+  
+  public void region5Mission1ButtonPressed()
+  {
+	  missionSelectionFrame.dispose();
+	  new MissionSelection(frameSize);
+  }
+  
+  public JPanel addRegion1MissionPanel()
+  {
+	  region1Mission1Button = new JButton("Mission 1");
+	  region1Title = new JLabel("Region 1");
+	  region1Title.setFont(new Font(region1Title.getName(), Font.BOLD, 18));
+	  region1Title.setForeground(java.awt.Color.white);
+	  maxButtonSize = region1Mission1Button.getPreferredSize();
+	  region1Mission1Button.addActionListener(new ActionListener() {
+		  public void actionPerformed(ActionEvent e) {
+			  region1Mission1ButtonPressed(); 
+	  		} 
+	  } );
+	  region1MissionPanel = new JPanel();
+	  region1MissionPanel.setOpaque(false);
+	  region1MissionPanel.add(region1Title);
+	  region1MissionPanel.add(region1Mission1Button);
+	  region1MissionPanel.setBounds((int) (frameSize.getWidth() / 8 - maxButtonSize.getWidth() / 2), (int) (frameSize.getHeight() / 8 - maxButtonSize.getHeight() / 2),
+	    		(int) (maxButtonSize.getWidth()), (int) ((maxButtonSize.getHeight())) * 3);
+	  return region1MissionPanel;
+  }
+  
+  public JPanel addRegion2MissionPanel()
+  {
+	  region2Mission1Button = new JButton("Mission 1");
+	  region2Title = new JLabel("Region 2");
+	  region2Title.setFont(new Font(region2Title.getName(), Font.BOLD, 18));
+	  region2Title.setForeground(java.awt.Color.white);
+	  maxButtonSize = region2Mission1Button.getPreferredSize();
+	  region2Mission1Button.addActionListener(new ActionListener() {
+		  public void actionPerformed(ActionEvent e) {
+			  region2Mission1ButtonPressed(); 
+	  		} 
+	  } );
+	  region2MissionPanel = new JPanel();
+	  region2MissionPanel.setOpaque(false);
+	  region2MissionPanel.add(region2Title);
+	  region2MissionPanel.add(region2Mission1Button);
+	  region2MissionPanel.setBounds((int) (frameSize.getWidth() / 5 - maxButtonSize.getWidth() / 2), (int) (frameSize.getHeight() / 2 - maxButtonSize.getHeight() / 2),
+	    		(int) (maxButtonSize.getWidth()), (int) ((maxButtonSize.getHeight())) * 3);
+	  return region2MissionPanel;
+  }
+  
+  public JPanel addRegion3MissionPanel()
+  {
+	  region3Mission1Button = new JButton("Mission 1");
+	  region3Title = new JLabel("Region 3");
+	  region3Title.setFont(new Font(region3Title.getName(), Font.BOLD, 18));
+	  region3Title.setForeground(java.awt.Color.white);
+	  maxButtonSize = region3Mission1Button.getPreferredSize();
+	  region3Mission1Button.addActionListener(new ActionListener() {
+		  public void actionPerformed(ActionEvent e) {
+			  region3Mission1ButtonPressed(); 
+	  		} 
+	  } );
+	  region3MissionPanel = new JPanel();
+	  region3MissionPanel.setOpaque(false);
+	  region3MissionPanel.add(region3Title);
+	  region3MissionPanel.add(region3Mission1Button);
+	  region3MissionPanel.setBounds((int) (frameSize.getWidth() / 2 - maxButtonSize.getWidth() / 2 - 30), (int) (frameSize.getHeight() / 2 - maxButtonSize.getHeight() / 2 - 50),
+	    		(int) (maxButtonSize.getWidth()), (int) ((maxButtonSize.getHeight())) * 3);
+	  return region3MissionPanel;
+  }
+  
+  public JPanel addRegion4MissionPanel()
+  {
+	  region4Mission1Button = new JButton("Mission 1");
+	  region4Title = new JLabel("Region 4");
+	  region4Title.setFont(new Font(region4Title.getName(), Font.BOLD, 18));
+	  region4Title.setForeground(java.awt.Color.white);
+	  maxButtonSize = region4Mission1Button.getPreferredSize();
+	  region4Mission1Button.addActionListener(new ActionListener() {
+		  public void actionPerformed(ActionEvent e) {
+			  region4Mission1ButtonPressed(); 
+	  		} 
+	  } );
+	  region4MissionPanel = new JPanel();
+	  region4MissionPanel.setOpaque(false);
+	  region4MissionPanel.add(region4Title);
+	  region4MissionPanel.add(region4Mission1Button);
+	  region4MissionPanel.setBounds((int) (frameSize.getWidth() / 2 - maxButtonSize.getWidth() / 2 + 100), (int) (frameSize.getHeight() / 8 - maxButtonSize.getHeight() / 2),
+	    		(int) (maxButtonSize.getWidth()), (int) ((maxButtonSize.getHeight())) * 3);
+	  return region4MissionPanel;
+  }
+  
+  public JPanel addRegion5MissionPanel()
+  {
+	  region5Mission1Button = new JButton("Mission 1");
+	  region5Title = new JLabel("Region 5");
+	  region5Title.setFont(new Font(region5Title.getName(), Font.BOLD, 18));
+	  region5Title.setForeground(java.awt.Color.white);
+	  maxButtonSize = region5Mission1Button.getPreferredSize();
+	  region5Mission1Button.addActionListener(new ActionListener() {
+		  public void actionPerformed(ActionEvent e) {
+			  region5Mission1ButtonPressed(); 
+	  		} 
+	  } );
+	  region5MissionPanel = new JPanel();
+	  region5MissionPanel.setOpaque(false);
+	  region5MissionPanel.add(region5Title);
+	  region5MissionPanel.add(region5Mission1Button);
+	  region5MissionPanel.setBounds((int) (frameSize.getWidth() - maxButtonSize.getWidth() / 2 - 300), (int) (frameSize.getHeight() / 2 - maxButtonSize.getHeight() / 2),
+	    		(int) (maxButtonSize.getWidth()), (int) ((maxButtonSize.getHeight())) * 3);
+	  return region5MissionPanel;
   }
 
 }
