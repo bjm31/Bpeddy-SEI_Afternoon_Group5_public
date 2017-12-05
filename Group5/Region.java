aab3b3b9deb388d1e601a098327a6da06f7d8b1a
Bryan Mort <32080274+bjm31@users.noreply.github.com>
2017-12-05 16:08:06 -0600

    Update Region.java
    updated all methods

--- Group5/Region.java
+++ Group5/Region.java
@@ -1,42 +1,68 @@
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
+
 public class Region {
+	
+	private JLabel regionImage;
 
-  public ArrayList<MapItem> mapItemList;
+	private ArrayList<MapItem> mapItemList;
 
-  public int reputation;
+	private ArrayList<Diplomat> diplomatList;
 
-  public ArrayList<Diplomat> diplomatList;
+	private ArrayList<Mission> missionList;
+  
+	private int reputation;
 
-  public ArrayList<Mission> missionList;
-
-  public void Region( ArrayList<Diplomat>,  ArrayList<MapItem>,  ArrayList<Mission>,  int) {
+  public Region(JLabel regionImage, ArrayList<Diplomat> diplomatList, ArrayList<MapItem> mapItemList, ArrayList<Mission> missionList, int reputation) {
+	  this.regionImage = regionImage;
+	  this.diplomatList = diplomatList;
+	  this.mapItemList = mapItemList;
+	  this.missionList = missionList;
+	  this.reputation = reputation;
   }
 
   public ArrayList<MapItem> getMapItemList() {
-  return null;
+	  return mapItemList;
   }
 
-  public void setMapItemList( ArrayList<MapItem>) {
+  public void setMapItemList(ArrayList<MapItem> mapItemList) {
+	  this.mapItemList = mapItemList;
   }
 
   public int getReputation() {
-  return 0;
+	  return reputation;
   }
 
-  public void setReputation( int) {
+  public void setReputation(int reputation) {
   }
 
   public ArrayList<Diplomat> getDiplomatList() {
-  return null;
+	  return diplomatList;
   }
 
-  public void setDiplomatList( ArrayList<Diplomat>) {
+  public void setDiplomatList(ArrayList<Diplomat> diplomatList) {
+	  this.diplomatList = diplomatList;
   }
 
-  public ArrayLIst<Mission> getMissionList() {
-  return null;
+  public ArrayList<Mission> getMissionList() {
+	  return missionList;
   }
 
-  public void setMissionList( ArrayList<Mission>) {
+  public void setMissionList(ArrayList<Mission> missionList) {
+	  this.missionList = missionList;
+  }
+  
+  public void updateReputation(int reputation) {
+	  this.reputation += reputation;
+  }
+
+  public JLabel getRegionImage() {
+	return regionImage;
+  }
+
+  public void setRegionImage(JLabel regionImage) {
+	this.regionImage = regionImage;
   }
 
 }
