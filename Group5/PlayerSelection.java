63b51fb986081061227bf477ec2947d9af49cb82
NicholasLookabaugh <32078028+NicholasLookabaugh@users.noreply.github.com>
2017-12-05 12:10:30 -0600

    Update PlayerSelection.java

--- Group5/PlayerSelection.java
+++ Group5/PlayerSelection.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -22,6 +23,13 @@ import java.io.File;
 public class PlayerSelection {
 	
   public JFrame selectionFrame;
+  
+  private JLabel title;
+  private JLabel label1;
+  private JLabel label2;
+  private JLabel label3;
+  
+  private JPanel selectionPanel;
 
   private JButton choice1;
 
@@ -30,6 +38,9 @@ public class PlayerSelection {
   private JButton choice3;
 
   private Dimension frameSize;
+  private Dimension titleSize;
+  
+  private GridBagConstraints constraints;
   
   public PlayerSelection(JFrame jFrame, JButton jButton1, JButton jButton2, JButton jButton3) {
 
@@ -48,70 +59,9 @@ public class PlayerSelection {
   public void setSelectionFrame(Dimension frameSize) {
 	  selectionFrame = new JFrame("Player Selection");
 	  selectionFrame.setSize(frameSize);
-	  selectionFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	  
-	  choice1 = new JButton("Select");
-	  choice1.addActionListener(new ActionListener() 
-	  	{ public void actionPerformed(ActionEvent e) 
-	  		{ selectionButton1Pressed(); 
-	  	} 
-	  } );
-	  choice2 = new JButton("Select");
-	  choice2.addActionListener(new ActionListener() 
-	  	{ public void actionPerformed(ActionEvent e) 
-	  		{ selectionButton2Pressed(); 
-	  	} 
-	  } );
-	  choice3 = new JButton("Select");
-	  choice3.addActionListener(new ActionListener() 
-	  	{ public void actionPerformed(ActionEvent e) 
-	  		{ selectionButton3Pressed(); 
-	  	} 
-	  } );
-	  
-	  JPanel selectionPanel1 = new JPanel(new GridBagLayout());
-	  GridBagConstraints c = new GridBagConstraints();
-	  selectionPanel1.setBackground(java.awt.Color.black);
-	  JLabel label1 = new JLabel();
-	  JLabel label2 = new JLabel();
-	  JLabel label3 = new JLabel();
-	  try {
-		  label1 = new JLabel((Icon)new ImageIcon(ImageIO.read(new File ("alien1.jpg"))));
-		  label2 = new JLabel((Icon)new ImageIcon(ImageIO.read(new File ("alien2.jpg"))));
-		  label3 = new JLabel((Icon)new ImageIcon(ImageIO.read(new File ("alien3.jpg"))));
-	  }
-	  catch (IOException e)
-	  {
-		  e.printStackTrace();
-	  }
-	  c.insets = new Insets(0,25,0,0);
-	  c.weightx = 0.5;
-	  c.gridx = 0;
-	  c.gridy = 0;
-	  selectionPanel1.add(label1, c);
-	  c.weightx = 0.5;
-	  c.gridx = 1;
-	  c.gridy = 0;
-	  selectionPanel1.add(label2, c);
-	  c.weightx = 0.5;
-	  c.gridx = 2;
-	  c.gridy = 0;
-	  selectionPanel1.add(label3, c);
-	  c.insets = new Insets(25,0,0,0);
-	  c.weightx = 0.5;
-	  c.gridx = 0;
-	  c.gridy = 1;
-	  selectionPanel1.add(choice1, c);
-	  c.weightx = 0.5;
-	  c.gridx = 1;
-	  c.gridy = 1;
-	  selectionPanel1.add(choice2, c);
-	  c.weightx = 0.5;
-	  c.gridx = 2;
-	  c.gridy = 1;
-	  selectionPanel1.add(choice3, c);
-	  
-	  selectionFrame.add(selectionPanel1);
+	  selectionFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);	  
+	  selectionFrame.add(addTitle());
+	  selectionFrame.add(addSelectionPanel());
 	  selectionFrame.setVisible(true);
 	  System.out.println(selectionFrame.getSize().toString());
 	  
@@ -133,6 +83,85 @@ public class PlayerSelection {
   {
 	  selectionFrame.dispose();
 	  new ShipHubMenu(frameSize);
+  }
+  
+  public JPanel addSelectionPanel()
+  {
+	  choice1 = new JButton("Select");
+	  choice2 = new JButton("Select");
+	  choice3 = new JButton("Select");
+
+	  choice1.addActionListener(new ActionListener() 
+	  	{ public void actionPerformed(ActionEvent e) 
+	  		{ selectionButton1Pressed(); 
+	  	} 
+	  } );
+	  choice2.addActionListener(new ActionListener() 
+	  	{ public void actionPerformed(ActionEvent e) 
+	  		{ selectionButton2Pressed(); 
+	  	} 
+	  } );
+	  choice3.addActionListener(new ActionListener() 
+	  	{ public void actionPerformed(ActionEvent e) 
+	  		{ selectionButton3Pressed(); 
+	  	} 
+	  } );
+	  
+	  selectionPanel = new JPanel(new GridBagLayout());
+	  constraints = new GridBagConstraints();
+	  selectionPanel.setBackground(java.awt.Color.black);
+	  
+	  label1 = new JLabel();
+	  label2 = new JLabel();
+	  label3 = new JLabel();
+	  try {
+		  label1 = new JLabel((Icon)new ImageIcon(ImageIO.read(new File ("alien1.jpg"))));
+		  label2 = new JLabel((Icon)new ImageIcon(ImageIO.read(new File ("alien2.jpg"))));
+		  label3 = new JLabel((Icon)new ImageIcon(ImageIO.read(new File ("alien3.jpg"))));
+	  }
+	  catch (IOException e)
+	  {
+		  e.printStackTrace();
+	  }
+	  
+	  constraints.insets = new Insets(0,25,0,0);
+	  constraints.weightx = 0.5;
+	  constraints.gridx = 0;
+	  constraints.gridy = 0;
+	  selectionPanel.add(label1, constraints);
+	  constraints.weightx = 0.5;
+	  constraints.gridx = 1;
+	  constraints.gridy = 0;
+	  selectionPanel.add(label2, constraints);
+	  constraints.weightx = 0.5;
+	  constraints.gridx = 2;
+	  constraints.gridy = 0;
+	  selectionPanel.add(label3, constraints);
+	  constraints.insets = new Insets(25,0,0,0);
+	  constraints.weightx = 0.5;
+	  constraints.gridx = 0;
+	  constraints.gridy = 1;
+	  selectionPanel.add(choice1, constraints);
+	  constraints.weightx = 0.5;
+	  constraints.gridx = 1;
+	  constraints.gridy = 1;
+	  selectionPanel.add(choice2, constraints);
+	  constraints.weightx = 0.5;
+	  constraints.gridx = 2;
+	  constraints.gridy = 1;
+	  selectionPanel.add(choice3, constraints);
+	  return selectionPanel;
+  }
+  
+  public JLabel addTitle()
+  {
+	  title = new JLabel("Character Selection");
+	  title.setFont(new Font(title.getName(), Font.BOLD, 50));
+	  title.setForeground(java.awt.Color.white);
+	  titleSize = title.getPreferredSize();
+	  title.setBounds((int) (frameSize.getWidth() / 2 - titleSize.getWidth() / 2), (int) frameSize.getHeight() / 90,
+				(int) titleSize.getWidth(), (int) titleSize.getHeight());
+	  return title;
   }
 
 }
