394b868ca7feae0e1834703a5e2b1fed91814977
bryan <bryan@192.168.0.43>
2017-12-05 21:02:09 -0600

    created playing environment frame with grid

--- Group5/PlayingEnvironment.java
+++ Group5/PlayingEnvironment.java
@@ -1,19 +1,22 @@
 import java.awt.Dimension;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class PlayingEnvironment {
 
   private Region region;
 
   private Dimension frameSize;
-
-  private JLabel regionImage;
   
-  public PlayingEnvironment(Region region, Dimension frameSize, JLabel regionImage) {
+  public PlayingEnvironment(Region region, Dimension frameSize) {
 	  this.region = region;
 	  this.frameSize = frameSize;
 	  this.regionImage = regionImage;
+	  
+	  createPlayingEnvironment();
   }
 
   public Region getRegion() {
@@ -24,20 +27,31 @@ public class PlayingEnvironment {
 	  this.region = region;
   }
 
-  public JLabel getRegionImage() {
-	  return regionImage;
-  }
-
-  public void setRegionImage(JLabel regionImage) {
-	  this.regionImage = regionImage;
-  }
-
   public Dimension getFrameSize() {
 	  return frameSize;
   }
 
   public void setFrameSize(Dimension frameSize) {
 	  this.frameSize = frameSize;
+  }
+  
+  public void createPlayingFrame() {
+	  
+	  JFrame playingFrame = new JFrame();
+	  playingFrame.setSize(getFrameSize());
+	  
+	  GridLayout gridLayout = new GridLayout(10,20);
+	  
+	  for (int r = 1; r <= gridLayout.getRows(); r++) {
+		  for int (c = 1; c <= gridLayout.getColumns(); c++) {
+			  JPanel cellPanel = new JPanel();
+			  JButton cellButton = new JButton(r + "," + c);
+			  cellPanel.add(cellButton);
+		  }
+	  }
+	  
+	  playingFrame.add(gridLayout);
+	  playingFrame.setVisible(true);
   }
 
 }
