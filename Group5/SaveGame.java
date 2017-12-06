1011f28e2c4f77dbca78ce8a1e404b35f711b1d8
NicholasLookabaugh <32078028+NicholasLookabaugh@users.noreply.github.com>
2017-12-05 20:26:49 -0600

    Update SaveGame.java

--- Group5/SaveGame.java
+++ Group5/SaveGame.java
@@ -1,43 +1,142 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 public class SaveGame {
+	
+	  private Player player;
+	  
+	  private Dimension frameSize;
+	  
+	  private JFrame saveGameFrame;
 
-  private String fileName;
+	  private JButton backButton;
 
-  private ShipHubMenu shipHubMenu;
+	private Dimension maxButtonSize;
 
-  private JFrame saveGameFrame;
+	private JPanel backPanel;
 
-  private JOptionPane saveGameOptions;
+	private JButton saveAndContinueButton;
 
-  public void SaveGame( String,  ShipHubMenu,  JFrame,  JOptionPane) {
-  }
+	private JButton saveAndMainButton;
 
-  public String getFileName() {
-  return null;
-  }
+	private JButton saveAndExitButton;
 
-  public void setFileName( String) {
-  }
+	private JPanel buttonPanel;
+	  
+	  public SaveGame(Dimension frameSize, Player player) {
+			this.player = player;
+			this.frameSize = frameSize;
+			setSaveGameFrame(frameSize);
+		}
+	  
+	  public void setSaveGameFrame(Dimension frameSize) {
+		  	saveGameFrame = new JFrame("Force Selection");
+		  	saveGameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			try {
+				saveGameFrame.setContentPane(new JLabel((Icon) new ImageIcon(ImageIO.read(new File("ship.jpg")))));
+				} catch (IOException e) {
+					e.printStackTrace();
+			}
+			saveGameFrame.pack();
 
-  public ShipHubMenu getShipHubMenu() {
-  return null;
-  }
+			saveGameFrame.setVisible(true);
+			saveGameFrame.setSize(frameSize);
 
-  public void setShipHubMenu( ShipHubMenu) {
-  }
-
-  public JFrame getSaveGameFrame() {
-  return null;
-  }
-
-  public void setSaveGameFrame( JFrame) {
-  }
-
-  public JOptionPane getSaveGameOptions() {
-  return null;
-  }
-
-  public void setSaveGameOptions( JOptionPane) {
-  }
+			saveGameFrame.add(addBackPanel());
+			saveGameFrame.add(addButtonPanel());
+		  
+			System.out.println(saveGameFrame.getSize().toString());
+		}
+	  
+	  public void backButtonPressed()
+	  {
+		  saveGameFrame.dispose();
+		  new ShipHubMenu(frameSize, player);
+	  }
+	  
+	  public void saveAndContinueButtonPressed()
+		{
+		  saveGameFrame.dispose();
+			new SaveGame(frameSize, player);
+		}
+	  
+	  public void saveAndMainButtonPressed()
+		{
+		  	saveGameFrame.dispose();
+			new GameLaunch();
+		}
+	  
+	  public void saveAndExitButtonPressed()
+		{
+			saveGameFrame.dispose();
+			System.exit(0);
+		}
+	  
+	  public JPanel addButtonPanel()
+		{
+			saveAndContinueButton = new JButton("Save and Continue");
+			saveAndMainButton = new JButton("Save and go to Main Menu");
+			saveAndExitButton = new JButton("Save and Exit");
+		  
+			maxButtonSize = saveAndMainButton.getPreferredSize();
+			saveAndExitButton.setPreferredSize(maxButtonSize);
+			saveAndContinueButton.setPreferredSize(maxButtonSize);
+		  
+			saveAndContinueButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					saveAndContinueButtonPressed(); 
+				} 
+			} );
+			saveAndMainButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					saveAndMainButtonPressed(); 
+				} 
+			} );
+			saveAndExitButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					saveAndExitButtonPressed(); 
+				} 
+			} );
+		  
+			buttonPanel = new JPanel();
+			buttonPanel.setOpaque(false);
+			buttonPanel.add(saveAndContinueButton);
+			buttonPanel.add(saveAndMainButton);
+			buttonPanel.add(saveAndExitButton);
+			buttonPanel.setBounds((int) (frameSize.getWidth() / 2 - maxButtonSize.getWidth() / 2), (int) (frameSize.getHeight() / 2 - maxButtonSize.getHeight() / 2),
+					(int) (maxButtonSize.getWidth()), (int) ((maxButtonSize.getHeight())) * 5);
+			return buttonPanel;
+		}
+	  
+	  public JPanel addBackPanel()
+	  {
+		  backButton = new JButton("Back");
+		  maxButtonSize = backButton.getPreferredSize();
+		  backButton.addActionListener(new ActionListener() {
+			  public void actionPerformed(ActionEvent e) {
+				  backButtonPressed();
+			  }
+		  } );
+		  backPanel = new JPanel();
+		  backPanel.setOpaque(false);
+		  backPanel.add(backButton);
+		  backPanel.setBounds((int) (frameSize.getWidth() - maxButtonSize.getWidth() / 2 - 50), (int) (frameSize.getHeight() - maxButtonSize.getHeight() / 2 - 60),
+		    		(int) (maxButtonSize.getWidth()), (int) ((maxButtonSize.getHeight())) * 3);
+		  return backPanel;
+	  }
 
 }
