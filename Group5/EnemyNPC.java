b55dd73281ae4ff2a864fb492f0e0de7e91cd673
NicholasLookabaugh <32078028+NicholasLookabaugh@users.noreply.github.com>
2017-12-10 21:19:21 -0600

    Update EnemyNPC.java

--- Group5/EnemyNPC.java
+++ Group5/EnemyNPC.java
@@ -1,13 +1,10 @@
 import javax.swing.Icon;
-import javax.swing.JLabel;
 
 public class EnemyNPC extends MapItem {
 
   private Force force;
 
   private Icon image;
-  
-  private int experience;
 
   public EnemyNPC(int xPos, int yPos, Icon image, Force force) {
 	  super(xPos,yPos);
